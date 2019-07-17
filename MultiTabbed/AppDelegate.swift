a00c17a459a6e7655b76235fff2a427e7da511eb
Christian Tietze <christian.tietze@gmail.com>
2019-07-17 11:02:42 +0200

    Initial Commit

---
+++ MultiTabbed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MultiTabbed
+//
+//  Created by Christian on 17.07.19.
+//  Copyright © 2019 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MultiTabbed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MultiTabbed
+//
+//  Created by Christian on 17.07.19.
+//  Copyright © 2019 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
