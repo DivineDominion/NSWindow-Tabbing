3046579584981d31c4f13a03f17997394045c457
Christian Tietze <christian.tietze@gmail.com>
2019-07-20 09:43:29 +0200

    fix issues with tabbing after the initial window closes

--- MultiTabbed/WindowController.swift
+++ MultiTabbed/WindowController.swift
@@ -14,7 +14,7 @@ class WindowController: NSWindowController {
 
     override func newWindowForTab(_ sender: Any?) {
 
-        guard let mainWindow = self.window else {
+        guard let mainKeyWindow = self.window else {
             preconditionFailure("Expected window to be loaded")
         }
 
@@ -22,12 +22,33 @@ class WindowController: NSWindowController {
         let newWindow = newWindowController.window!
 
         // Add as a new tab right to the current one
-        mainWindow.addTabbedWindow(newWindow, ordered: .above)
+        mainKeyWindow.addTabbedWindow(newWindow, ordered: .above)
+        newWindow.delegate = self // Set delegate first to notify about key window changes
         newWindow.makeKeyAndOrderFront(self)
 
         // `newWindowController` is not referenced and will be deallocated at the end of
         // this method, so use 1 shared controller to keep the window in the responder chain.
         newWindow.windowController = self
+
+        inspectWindowHierarchy()
     }
 
+    func windowDidBecomeKey(_ notification: Notification) {
+        guard let window = notification.object as? NSWindow else { return }
+        guard window != self.window else { return }
+        let oldFrame = self.window?.frame
+        self.window = window
+        inspectWindowHierarchy()
+        if let oldFrame = oldFrame {
+            window.setFrame(oldFrame, display: true)
+        }
+    }
+
+    func inspectWindowHierarchy() {
+        let rootWindow = self.window!
+        print("Root window", rootWindow, rootWindow.title, "has tabs:")
+        rootWindow.tabbedWindows?.forEach { window in
+            print("- ", window, window.title, "isKey =", window.isKeyWindow, ", isMain =", window.isMainWindow, " at ", window.frame)
+        }
+    }
 }
