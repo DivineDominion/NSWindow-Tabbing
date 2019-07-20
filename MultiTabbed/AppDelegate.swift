401bbfd2d98a043e57baa22204e97867057887e8
Christian Tietze <christian.tietze@gmail.com>
2019-07-20 10:21:59 +0200

    add "New Tab" menu bar item that also works when no window is visible

--- MultiTabbed/AppDelegate.swift
+++ MultiTabbed/AppDelegate.swift
@@ -2,21 +2,36 @@
 
 import Cocoa
 
+private func createMainController() -> WindowController {
+    let windowStoryboard = NSStoryboard(name: "WindowController", bundle: nil)
+    return windowStoryboard.instantiateInitialController() as! WindowController
+}
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var tabService: TabService!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let windowStoryboard = NSStoryboard(name: "WindowController", bundle: nil)
-        guard let mainController = windowStoryboard.instantiateInitialController() as? WindowController else { preconditionFailure() }
-
-        mainController.showWindow(nil)
-        tabService = TabService(initialWindowController: mainController)
+        replaceTabService()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+
+    /// Fallback for the menu bar action when all windows are closed.
+    @IBAction func newWindowForTab(_ sender: Any?) {
+
+        // Not supposed to be called while another window is visible
+        precondition(tabService.managedWindows.isEmpty)
+        replaceTabService()
+    }
+
+    private func replaceTabService() {
+        let windowController = createMainController()
+        windowController.showWindow(self)
+        tabService = TabService(initialWindowController: windowController)
     }
 
 }
